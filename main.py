bc64078e332248ed3ad876fd55cf8f0f09fd69a8
Darkhan Kumysbekov <darkhankumysbekov@Darkhans-Air.home>
2026-01-13 19:31:17 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/hoot")
+def hello():
+    data = ["asfdasdf sdf asdf",2,3,4,5,6]
+    return data
+
+
+app.run(debug=True, use_debugger=True, use_reloader=True)
